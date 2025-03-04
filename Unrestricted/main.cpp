60c645d72be8d16c7cdde503517bb647a6ab1145
Your Name <your.email@example.com>
2025-03-05 01:08:25 +0800

    調整main測試格式

--- Unrestricted/main.cpp
+++ Unrestricted/main.cpp
@@ -19,42 +19,21 @@ int main() {
     }
     */
     initializePrecomputed();
-    int gameTimes = 5;
+    int gameTimes = 30;
     long long totalTime = 0;
-    ofstream outputFile("bitboard_main_threadV1.csv");
+    ofstream outputFile("row_col.csv");
     if (!outputFile.is_open()) {
         cerr << "Error: Unable to open output file!" << endl;
         return 1;
     }
     // 寫入 CSV 標題行
-    outputFile << "Core,SimulationTimes,AverageTime (ms)" << endl;
-    // 模擬不同次數的情況
-    for (int i = 6; i <= 6; i++) {
-        for (int simulationTimes = 1000; simulationTimes <= 5000; simulationTimes += 1000) {
-            totalTime = 0;
-            for (int j = 0; j < gameTimes; j++) {
-                Node* root = new Node();  // 創建根節點
-                // Game::generateFullTree(root);        // 生成完整遊戲樹
-                MCTS ai(simulationTimes, i);       // 創建 MCTS AI
-                totalTime += ai.run(root, 10000);  // 執行 MCTS
-                deleteTree(root);                  // 刪除樹
-            }
-            // 計算平均時間
-            double average_time = totalTime / static_cast<double>(gameTimes);
-            // 輸出到 CSV 檔案
-            outputFile << i << "," << simulationTimes << "," << average_time << endl;
-            // 同時輸出到控制台（可選）
-            cout << "Core = " << i << ", SimulationTimes = " << simulationTimes << ", AverageTime = " << average_time
-                 << " ms" << endl;
-        }
-    }
-    /*
+    outputFile << "SimulationTimes,AverageTime (ms)" << endl;
     for (int simulationTimes = 1000; simulationTimes <= 10000; simulationTimes += 1000) {
         totalTime = 0;
         for (int i = 0; i < gameTimes; i++) {
             Node* root = new Node();  // 創建根節點
             // Game::generateFullTree(root);        // 生成完整遊戲樹
-            MCTS ai(simulationTimes, 5);       // 創建 MCTS AI
+            MCTS ai(simulationTimes, 6);       // 創建 MCTS AI
             totalTime += ai.run(root, 10000);  // 執行 MCTS
             deleteTree(root);                  // 刪除樹
         }
@@ -67,6 +46,5 @@ int main() {
     }
     // 關閉檔案
     outputFile.close();
-    */
     return 0;
 }
